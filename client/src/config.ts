f80648b2adf47eb7e0c24a6708e909a80166a066
Mahmood <49205684+Brainiac-M@users.noreply.github.com>
2022-08-31 05:30:43 +0100

    project done and files uploaded

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+const apiId = '4va0vpqq44';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'foxcoder508.us.auth0.com',
+  clientId: 'TnQQ9ZZ1XEc6PMooF6IIwGPmjxErveuK',
   callbackUrl: 'http://localhost:3000/callback'
 };
